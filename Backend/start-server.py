efa5134649a534bb7a2d82400a71bcbdcafd54ae
Alexander Kryklia <kryklia@gmail.com>
2014-02-03 14:42:25 +0200

    Add static support.

--- Backend/start-server.py
+++ Backend/start-server.py
@@ -1,14 +1,18 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route("/")
-def hello():
+def index():
+    return app.send_static_file('index.html')
+
+@app.route("/schedule")
+def schedule():
     data = ""
     try:
-    with open("schedule.json", "r") as myfile:
-        data=myfile.read()
+        with open("schedule.json", "r") as myfile:
+            data=myfile.read()
     except Exception, e:
-    print "Exception: ", e
+        data = 'Exception in schedule: {}'.format(e.msg)
     return data
 
 if __name__ == "__main__":
