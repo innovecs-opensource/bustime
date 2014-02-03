e151e9a09421874c17c97e2f1e46e5ef878985e7
Alexander Kryklia <kryklia@gmail.com>
2014-02-03 15:01:37 +0200

    Remove commented lines.

--- Backend/start.py
+++ Backend/start.py
@@ -3,7 +3,6 @@ app = Flask(__name__, static_url_path='')
 
 @app.route("/")
 def index():
-    # return app.send_static_file('index.html')
     return render_template('index.html')
 
 @app.route("/schedule")
